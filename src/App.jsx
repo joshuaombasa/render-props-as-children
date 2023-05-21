c716d75cd8b7365d00cbccb802120e058076b491
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-21 20:39:13 +0300

    added jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Host from './Host'
+import Header from './Header'
+function App() {
+  
+  return (
+    <div>
+     <Header/>
+     <Host/>
+    </div>
+  )
+}
+
+export default App

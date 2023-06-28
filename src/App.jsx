02e620cbdda25051b732c354200a612b364fe6bc
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-28 23:39:43 +0300

    updated render props logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import Host from './Host'
 import Header from './Header'
 function App() {
-  
+
   return (
     <div>
-     <Header/>
-     <Host/>
+      <Header />
+      <Host />
     </div>
   )
 }
